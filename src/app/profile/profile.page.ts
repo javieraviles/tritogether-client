4784257033cb020d97cdfb50b6493cdc00b916a4
Javier Aviles <avileslopez.javier@gmail.com>
2018-12-25 14:12:12 +0100

    Logout within profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -9,7 +10,7 @@ export class ProfilePage implements OnInit {
 
   currentUser: any = null;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
